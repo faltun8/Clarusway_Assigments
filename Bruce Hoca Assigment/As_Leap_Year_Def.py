039877a120deb439a637a98e3febb2de70c158dc
furkan <frknaltn08@gmail.com>
2020-07-20 00:37:52 -0400

    Leap Year added

---
+++ Bruce Hoca Assigment/As_Leap_Year_Def.py
@@ -0,0 +1,9 @@
+year = int(input("Please enter a year which is you want to check: "))
+if year % 400 == 0:
+    print(f"{year} is a leap year")
+elif year % 100 == 0:
+    print(f"{year} is not a leap year")
+elif year % 4 == 0:
+    print(f"{year} is a leap year")
+else:
+    print(f"{year} is not a leap year")
